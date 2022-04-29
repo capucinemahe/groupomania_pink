75b9b57a7ae007c0652caec86e6bf88e07faefef
CapucineMahe <capucine.mahe.oc@gmail.com>
2022-04-29 15:54:28 +0200

    deploy

--- frontend/pages/404.js
+++ frontend/pages/404.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     return (
         <div className={styles.NF_container}>
             <h1 className={styles.NF_title}>404 NOT FOUND</h1>
-            <h2>Sorry, cette page n'a pas été trouvée</h2>
+            <h2>Sorry, cette page n'a pas été trouvée ! </h2>
             <Link href="/" className={styles.NF_title}>Clique ici pour retourner sur la page d'accueil</Link>
         </div>
     );
